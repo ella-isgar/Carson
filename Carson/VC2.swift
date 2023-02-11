7b20f4177ec3e65c0afc03c067d5b0c63f187f5d
ella-isgar <ellar-isgar@gmail.com>
2023-02-11 01:17:49 -0500

    Initial Commit

---
+++ Carson/VC2.swift
@@ -0,0 +1,23 @@
+//
+//  VC2.swift
+//  Carson
+//
+//  Created by Ella Isgar on 2/11/23.
+//
+
+import UIKit
+
+class VC2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+    }*/
+    
+}
---
+++ Carson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carson
+//
+//  Created by Ella Isgar on 2/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

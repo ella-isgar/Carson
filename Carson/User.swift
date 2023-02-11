c9d30989805c59bcfb90e2e9b8067c3d9110f643
annikachang <chang.ann@northeastern.edu>
2023-02-11 16:08:58 -0500

    session RED to green!

--- Carson/User.swift
+++ Carson/User.swift
@@ -8,15 +8,19 @@
 import Foundation
 
 class User {
+    var start: String
+    var dest: String
     var username: String
     var password: String
     var name: String
     var pronouns: String
-    var phoneNum: Int
+    var phoneNum: String
     var organization: String
     var age: Int
     
-    init(username: String, password: String, name: String, pronouns: String, phoneNum: Int, organization: String, age: Int) {
+    init(start: String, dest: String, username: String, password: String, name: String, pronouns: String, phoneNum: String, organization: String, age: Int) {
+        self.start = start
+        self.dest = dest
         self.username = username
         self.password = password
         self.name = name
